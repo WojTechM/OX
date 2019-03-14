6f75dd8b37601124f8a6d60295b11dd4ceee78ee
WojTechM <wojciech.m1997@gmail.com>
2019-03-14 11:37:42 +0100

    Create uml.dia - planned base classes for the project

---
+++ src/main/java/com/github/wojtechm/Main.java
@@ -0,0 +1,11 @@
+package com.github.wojtechm;
+
+/**
+ * @author Makiela Wojciech
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
