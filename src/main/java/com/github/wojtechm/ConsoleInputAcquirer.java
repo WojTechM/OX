3b34a165621831f579a328e51c77e3762b84f082
WojTechM <wojciech.m1997@gmail.com>
2019-03-19 17:45:47 +0100

    Create tests and implementation of ConsoleInputAcquirer

--- src/main/java/com/github/wojtechm/ConsoleInputAcquirer.java
+++ src/main/java/com/github/wojtechm/ConsoleInputAcquirer.java
@@ -1,7 +1,37 @@
 package com.github.wojtechm;
 
+import java.util.Scanner;
+
 /**
  * @author Makiela Wojciech
  */
-public class ConsoleInputAcquirer {
+class ConsoleInputAcquirer {
+
+    private Scanner scanner;
+
+    ConsoleInputAcquirer(Scanner scanner) {
+        if (scanner == null) {
+            throw new IllegalArgumentException("Console Input Acquirer cannot read from null");
+        }
+        this.scanner = scanner;
+    }
+
+    int getIntFromUser() {
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+        }
+        return scanner.nextInt();
+    }
+
+    int getIntInRangeFromUser(int from, int to) {
+        int input = from -1;
+        while (input < from || input > to) {
+            input = getIntFromUser();
+        }
+        return input;
+    }
+
+    String getStringFromUser() {
+        return scanner.next();
+    }
 }
---
+++ src/test/java/com/github/wojtechm/ConsoleInputAcquirerTest.java
@@ -0,0 +1,100 @@
+package com.github.wojtechm;
+
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+import static org.testng.Assert.*;
+
+/**
+ * @author Makiela Wojciech
+ */
+public class ConsoleInputAcquirerTest {
+
+    private Random random = new Random();
+
+    @Test
+    private void Should_CreateInstance_When_PassedValidArgs_Otherwise_ThrowException() {
+        new ConsoleInputAcquirer(new Scanner(System.in));
+        assertThrows(IllegalArgumentException.class, () -> new ConsoleInputAcquirer(null));
+    }
+
+    @Test(invocationCount = 10)
+    private void Should_ReturnInt_When_CalledAskUserForAnInteger() {
+        StringBuilder stringBuilder = new StringBuilder();
+        String letters = "qwertyuiopasdfghjklzxcvbnm";
+        for (int n = 0; n < random.nextInt(6) + 1; n++) {
+            stringBuilder.append(letters.charAt(random.nextInt(letters.length())));
+            stringBuilder.append("\n");
+        }
+
+        int expected = random.nextInt(Integer.MAX_VALUE);
+
+        stringBuilder.append(expected);
+        stringBuilder.append("\n");
+
+        for (int n = 0; n < random.nextInt(6) + 1; n++) {
+            stringBuilder.append(letters.charAt(random.nextInt(letters.length())));
+            stringBuilder.append("\n");
+        }
+
+        Scanner scanner = new Scanner(stringBuilder.toString());
+        int actual = new ConsoleInputAcquirer(scanner).getIntFromUser();
+        assert expected == actual;
+    }
+
+    @Test(invocationCount = 10)
+    private void Should_ReturnIntFromGivenRange_When_CalledGetIntInRangeFromUser() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int min = random.nextInt(100);
+        int max = min + 1 + random.nextInt(100);
+        String letters = "qwertyuiopasdfghjklzxcvbnm";
+        for (int n = 0; n < random.nextInt(6) + 1; n++) {
+            stringBuilder.append(letters.charAt(random.nextInt(letters.length())));
+            stringBuilder.append("\n");
+        }
+
+        for (int n = 0; n < random.nextInt(6) + 1; n++) {
+            stringBuilder.append(random.nextInt(200) + max);
+            stringBuilder.append("\n");
+        }
+
+
+        int expected = random.nextInt(max - min) + 1 + min;
+
+        stringBuilder.append(expected);
+        stringBuilder.append("\n");
+
+        Scanner scanner = new Scanner(stringBuilder.toString());
+        int actual = new ConsoleInputAcquirer(scanner).getIntInRangeFromUser(min, max);
+        assert expected == actual : String.format("Expected %s, got %s", expected, actual);
+    }
+
+    @Test(invocationCount = 10)
+    private void Should_ReturnString_When_CalledGetIntInRangeFromUser() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int min = random.nextInt(100);
+        int max = min + 1 + random.nextInt(100);
+        String letters = "qwertyuiopasdfghjklzxcvbnm";
+        for (int n = 0; n < random.nextInt(6) + 1; n++) {
+            stringBuilder.append(letters.charAt(random.nextInt(letters.length())));
+            stringBuilder.append("\n");
+        }
+
+        for (int n = 0; n < random.nextInt(6) + 1; n++) {
+            stringBuilder.append(random.nextInt(200) + max);
+            stringBuilder.append("\n");
+        }
+
+
+        int expected = random.nextInt(max - min) + 1 + min;
+
+        stringBuilder.append(expected);
+        stringBuilder.append("\n");
+
+        Scanner scanner = new Scanner(stringBuilder.toString());
+        int actual = new ConsoleInputAcquirer(scanner).getIntInRangeFromUser(min, max);
+        assert expected == actual : String.format("Expected %s, got %s", expected, actual);
+    }
+}
