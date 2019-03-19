bbc16edcf78922865cdfc09b2b78e1b5eee64f6b
WojTechM <wojciech.m1997@gmail.com>
2019-03-19 12:07:31 +0100

    Implement Point class in accordance with tests

--- src/main/java/com/github/wojtechm/Point.java
+++ src/main/java/com/github/wojtechm/Point.java
@@ -1,8 +1,39 @@
 package com.github.wojtechm;
 
+import java.util.Objects;
+
 /**
  * @author Makiela Wojciech
  */
-public class Point {
+class Point {
 
+    final int x;
+    final int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
