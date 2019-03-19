ee26d57e654209cc4b0bab7e63810250df0614ed
WojTechM <wojciech.m1997@gmail.com>
2019-03-19 12:39:53 +0100

    Implement Board class in accordance with tests

--- src/main/java/com/github/wojtechm/Board.java
+++ src/main/java/com/github/wojtechm/Board.java
@@ -1,9 +1,36 @@
 package com.github.wojtechm;
 
+import java.util.Arrays;
+
 /**
  * @author Makiela Wojciech
  */
-public class Board {
+class Board {
 
     private Field[][] fields;
+
+    Board(Field[][] fields) {
+        this.fields = fields;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return Arrays.equals(fields, board.fields);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(fields);
+    }
+
+    Field getMarkAtPoint(Point point) {
+        return fields[point.x][point.y];
+    }
+
+    void markPoint(Point point, Field mark) {
+        fields[point.x][point.y] = mark;
+    }
 }
