dc2ce301e1c4ccaffe6a3209315dfb65daab1050
WojTechM <wojciech.m1997@gmail.com>
2019-03-20 09:07:17 +0100

    Create stub implementation of Logger class

--- src/main/java/com/github/wojtechm/Logger.java
+++ src/main/java/com/github/wojtechm/Logger.java
@@ -3,6 +3,21 @@ package com.github.wojtechm;
 /**
  * @author Makiela Wojciech
  */
-public abstract class Logger {
+abstract class Logger {
 
+    private static Logger instance = null;
+
+    static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger() {
+                @Override
+                public void display(String string) {
+                    System.out.println(string);
+                }
+            };
+        }
+        return instance;
+    }
+
+    abstract void display(String string);
 }
