e3c009a01174631e05a91a5ecb6682de905cbb71
WojTechM <wojciech.m1997@gmail.com>
2019-03-20 09:09:38 +0100

    Implement main method - game now runs

--- src/main/java/com/github/wojtechm/Main.java
+++ src/main/java/com/github/wojtechm/Main.java
@@ -1,11 +1,19 @@
 package com.github.wojtechm;
 
+import java.util.Scanner;
+
 /**
  * @author Makiela Wojciech
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        ConsoleGameController consoleGameController = new ConsoleGameController(
+                new ConsolePlayerCreator(),
+                new ConsoleBoardCreator(),
+                new ConsoleInputAcquirer(new Scanner(System.in)));
+
+        consoleGameController.play();
     }
 }
