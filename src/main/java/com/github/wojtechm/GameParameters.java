a3533eee12c69edc9fab3ff6c1c7920881e8595c
WojTechM <wojciech.m1997@gmail.com>
2019-03-20 09:03:29 +0100

    Implement classes storing the game settings

--- src/main/java/com/github/wojtechm/GameParameters.java
+++ src/main/java/com/github/wojtechm/GameParameters.java
@@ -3,5 +3,27 @@ package com.github.wojtechm;
 /**
  * @author Makiela Wojciech
  */
-public class GameParameters {
+class GameParameters {
+
+    private int boardWidth = 3;
+    private int boardHeight = 3;
+    private int inlineMatches = 3;
+    private int numberOfRounds = 3;
+    private String translation = "polski";
+
+    String get(String parameter) {
+        switch (parameter) {
+            case "boardWidth":
+                return String.valueOf(boardWidth);
+            case "boardHeight":
+                return String.valueOf(boardHeight);
+            case "inlineMatches":
+                return String.valueOf(inlineMatches);
+            case "numberOfRounds":
+                return String.valueOf(numberOfRounds);
+            case "translation":
+                return String.valueOf(translation);
+        }
+        throw new IllegalArgumentException("Game parameters do not have variable: " + parameter);
+    }
 }
--- src/main/java/com/github/wojtechm/Settings.java
+++ src/main/java/com/github/wojtechm/Settings.java
@@ -3,8 +3,26 @@ package com.github.wojtechm;
 /**
  * @author Makiela Wojciech
  */
-public class Settings {
+class Settings {
+
+    private static Settings instance = null;
 
     private Translation translation;
     private GameParameters parameters;
+
+    private Settings(Translation translation, GameParameters parameters) {
+        this.translation = translation;
+        this.parameters = parameters;
+    }
+
+    static Settings getInstance() {
+        if (instance == null) {
+            instance = new Settings(new Translation(), new GameParameters());
+        }
+        return instance;
+    }
+
+    String getGameParameter(String parameter) {
+        return parameters.get(parameter);
+    }
 }
