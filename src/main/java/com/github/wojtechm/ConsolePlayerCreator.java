545d94a988d9d05012c94aa1d9247b031c8f89fe
WojTechM <wojciech.m1997@gmail.com>
2019-03-20 09:05:10 +0100

    Create stub implementation of ConsolePlayerCreator

--- src/main/java/com/github/wojtechm/ConsolePlayerCreator.java
+++ src/main/java/com/github/wojtechm/ConsolePlayerCreator.java
@@ -3,7 +3,12 @@ package com.github.wojtechm;
 /**
  * @author Makiela Wojciech
  */
-public class ConsolePlayerCreator implements PlayerCreator {
+class ConsolePlayerCreator implements PlayerCreator {
 
     private ConsoleInputAcquirer inputAcquirer;
+
+    @Override
+    public Players createPlayers() {
+        return new Players(new Player(new Field<>('o'), new Score(0)), new Player(new Field<>('x'), new Score(0)));
+    }
 }
--- src/main/java/com/github/wojtechm/PlayerCreator.java
+++ src/main/java/com/github/wojtechm/PlayerCreator.java
@@ -4,4 +4,5 @@ package com.github.wojtechm;
  * @author Makiela Wojciech
  */
 public interface PlayerCreator {
+    Players createPlayers();
 }
