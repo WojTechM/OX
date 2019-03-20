468366bc6ed637c16d59847ef0e2d95e929a1d64
WojTechM <wojciech.m1997@gmail.com>
2019-03-20 17:07:20 +0100

    Game settings now can be changed

--- src/main/java/com/github/wojtechm/ConsoleSettingsUpdate.java
+++ src/main/java/com/github/wojtechm/ConsoleSettingsUpdate.java
@@ -1,9 +1,33 @@
 package com.github.wojtechm;
 
+import java.util.MissingResourceException;
+
 /**
  * @author Makiela Wojciech
  */
-public class ConsoleSettingsUpdate extends SettingsUpdate {
+class ConsoleSettingsUpdate extends SettingsUpdate {
 
-    private ConsoleInputAcquirer inputAcquirer;
+    ConsoleSettingsUpdate(TranslationLoader translationLoader) {
+        super(translationLoader);
+    }
+
+    @Override
+    void loadTranslation(String requestedTranslation) {
+        try {
+            Translation translation = translationLoader.loadTranslation(requestedTranslation);
+            Settings.getInstance().setTranslation(translation);
+        } catch (MissingResourceException e) {
+            // TODO - Add communicate about nonexistent translation
+        }
+    }
+
+    @Override
+    boolean updateGameParameter(String parameter, String newValue) {
+        try {
+            Settings.getInstance().setGameParameter(parameter, newValue);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
 }
