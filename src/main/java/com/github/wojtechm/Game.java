fc5965b70deeb060cbc23bc7ccc17c4600fcd581
WojTechM <wojciech.m1997@gmail.com>
2019-03-20 09:08:12 +0100

    Implement game - runs single battle between players

--- src/main/java/com/github/wojtechm/Game.java
+++ src/main/java/com/github/wojtechm/Game.java
@@ -5,6 +5,35 @@ package com.github.wojtechm;
  */
 public class Game {
 
-    private Board board;
-    private Players players;
+    private final Board board;
+    private final Players players;
+    private final GameFinishedValidator validator;
+
+    public Game(Board board, Players players, GameFinishedValidator validator) {
+        this.board = board;
+        this.players = players;
+        this.validator = validator;
+    }
+
+    void update(Move move) throws IllegalMoveException, GameHasEndedException {
+
+        Field field = board.getMarkAtPoint(move.getPosition());
+        if (field != null) {
+            throw new IllegalMoveException();
+        }
+        board.markPoint(move.getPosition(), move.getMark());
+        if (validator.gameHasEnded(board, move)) {
+            throw new GameHasEndedException(getActivePlayer());
+        }
+
+        Logger.getInstance().display(board.toString());
+    }
+
+    Player getActivePlayer() {
+        return players.getCurrentPlayer();
+    }
+
+    void nextTurn() {
+        players.moveToNextPlayer();
+    }
 }
